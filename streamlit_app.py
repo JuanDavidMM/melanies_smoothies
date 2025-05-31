83868e408bde1883bda8647a5ebb69ec600b1ae9
Juan David Mata Martínez <133989708+JuanDavidMM@users.noreply.github.com>
2025-05-31 11:51:40 -0600

    Update streamlit_app.py
    indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,11 +41,10 @@ if ingredients_list:
     ingredients_string = ' '
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
